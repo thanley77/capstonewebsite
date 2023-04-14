30fbf98fe8b993c47c2a9e365b8a547e7ab160da
thanley77 <124112168+thanley77@users.noreply.github.com>
2023-04-14 18:12:17 -0400

    Update App.js

--- mern/client/src/App.js
+++ mern/client/src/App.js
@@ -5,7 +5,7 @@ import { Route, Routes } from "react-router-dom";
 
 // We import all the components we need in our app
 import Navbar from "./components/navbar";
-import HostnameList from "./components/hostnamesList";
+import HostnameList from "./components/recordList";
 import Edit from "./components/edit";
 import Create from "./components/create";
 
@@ -15,7 +15,7 @@ const App = () => {
       <Navbar />
       <div style={{ margin: 20 }}>
       <Routes>
-        <Route exact path="/" element={<HostnameList />} />
+        <Route exact path="/" element={<recordList />} />
         <Route path="/edit/:id" element={<Edit />} />
         <Route path="/create" element={<Create />} />
       </Routes>
