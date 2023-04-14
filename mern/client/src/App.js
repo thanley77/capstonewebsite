220183f24cbe125c29f8bf676ec773e266ba2e66
thanley77 <124112168+thanley77@users.noreply.github.com>
2023-04-14 18:17:17 -0400

    Update App.js

--- mern/client/src/App.js
+++ mern/client/src/App.js
@@ -5,7 +5,7 @@ import { Route, Routes } from "react-router-dom";
 
 // We import all the components we need in our app
 import Navbar from "./components/navbar";
-import HostnameList from "./components/recordList";
+import RecordList from "./components/recordList";
 import Edit from "./components/edit";
 import Create from "./components/create";
 
