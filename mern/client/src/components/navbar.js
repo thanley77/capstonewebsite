d6788dfe44ffb127d375769fc79683bec350df7f
thanley77 <124112168+thanley77@users.noreply.github.com>
2023-04-14 19:13:56 -0400

    Update navbar.js

--- mern/client/src/components/navbar.js
+++ mern/client/src/components/navbar.js
@@ -29,7 +29,7 @@ export default function Navbar() {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav ml-auto">
             <li className="nav-item">
-              <NavLink className="nav-link" to="/create">
+              <NavLink className="nav-link" to="/list">
                 Create Record
               </NavLink>
             </li>
