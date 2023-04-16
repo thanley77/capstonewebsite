a48d1e9e77d05f80012766a437b65dbf0a881a2d
thanley77 <124112168+thanley77@users.noreply.github.com>
2023-04-16 03:20:30 -0400

    Update navbar.js

--- mern/client/src/components/navbar.js
+++ mern/client/src/components/navbar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.css";
 
 export default function Navbar() {
   return (
@@ -7,7 +8,7 @@ export default function Navbar() {
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         {/* Navbar brand/logo */}
         <NavLink className="navbar-brand" to="/">
-          <img style={{ width: "25%" }} src="https://d3cy9zhslanhfa.cloudfront.net/media/3800C044-6298-4575-A05D5C6B7623EE37/4B45D0EC-3482-4759-82DA37D8EA07D229/webimage-8A27671A-8A53-45DC-89D7BF8537F15A0D.png" alt="MongoDB logo of green leaf and word MongoDB"></img>
+          <img style={{"width" : 25 + '%'}} src="https://d3cy9zhslanhfa.cloudfront.net/media/3800C044-6298-4575-A05D5C6B7623EE37/4B45D0EC-3482-4759-82DA37D8EA07D229/webimage-8A27671A-8A53-45DC-89D7BF8537F15A0D.png" alt="MongoDB logo of green leaf and word MongoDB"></img>
         </NavLink>
 
         {/* Navbar toggle button */}
