7f6d10a5dea0db341241dcd2bbd6ef1961a245f0
thanley77 <124112168+thanley77@users.noreply.github.com>
2023-04-16 00:50:29 -0400

    Update record.js

--- mern/server/routes/record.js
+++ mern/server/routes/record.js
@@ -14,7 +14,7 @@ const ObjectId = require("mongodb").ObjectId;
 
 // This section will help you get a list of all the records.
 recordRoutes.route("/record").get(function (req, res) {
-  let db_connect = dbo.getDb("pentests_ninjakiwi");
+  let db_connect = dbo.getDb("records");
   db_connect
     .collection("httpx")
     .find({})
