08cb377670b1013dfe786022f8225acc24788584
thanley77 <124112168+thanley77@users.noreply.github.com>
2023-04-19 19:13:15 -0400

    Update navbar.js

--- mern/client/src/components/navbar.js
+++ mern/client/src/components/navbar.js
@@ -44,6 +44,22 @@ export default function Navbar() {
               </NavLink>
             </li>
             {/* Add more links as needed */}
+            <li className="nav-item">
+              <form className="form-inline my-2 my-lg-0">
+                <input
+                  className="form-control mr-sm-2"
+                  type="search"
+                  placeholder="Search"
+                  aria-label="Search"
+                />
+                <button
+                  className="btn btn-outline-success my-2 my-sm-0"
+                  type="submit"
+                >
+                  Search
+                </button>
+              </form>
+            </li>
           </ul>
         </div>
       </nav>
