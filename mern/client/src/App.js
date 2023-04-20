ab814d328633506ea7c94cdafcb537c457f3173a
thanley77 <124112168+thanley77@users.noreply.github.com>
2023-04-19 20:11:22 -0400

    Update App.js

--- mern/client/src/App.js
+++ mern/client/src/App.js
@@ -8,6 +8,7 @@ import Navbar from "./components/navbar";
 import RecordList from "./components/recordList";
 import Edit from "./components/edit";
 import Create from "./components/create";
+import HttpxList from "./components/httpxList";
 
 const App = () => {
   return (
