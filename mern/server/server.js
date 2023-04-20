209ee0b3582c49a11b3d2f758e7d2a725e374d68
thanley77 <124112168+thanley77@users.noreply.github.com>
2023-04-19 23:30:31 -0400

    Update server.js

--- mern/server/server.js
+++ mern/server/server.js
@@ -1,3 +1,5 @@
+const express = require("express");
+const app = express();
 const cors = require("cors");
 require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 5000;
