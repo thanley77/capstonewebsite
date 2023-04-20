80b700daf9dc440bfc3b910a3b6d35191a4d2ccb
thanley77 <124112168+thanley77@users.noreply.github.com>
2023-04-19 22:28:52 -0400

    Update server.js

--- mern/server/server.js
+++ mern/server/server.js
@@ -1,23 +1,18 @@
-const express = require("express");
-const app = express();
 const cors = require("cors");
 require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 5000;
-const conn = require("./db/conn");
-
-// Connect to multiple databases
-conn.connectToServer("pentests_hyatt", function (err) {
-  if (err) throw err;
-});
-conn.connectToServer("pentests_ninjakiwi", function (err) {
-  if (err) throw err;
-});
-
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'))
 app.use(require("./routes/record"));
+// get driver connection
+const dbo = require("./db/conn");
 
 app.listen(port, () => {
+  // perform a database connection when server starts
+  dbo.connectToServer(function (err) {
+    if (err) console.error(err);
+
+  });
   console.log(`Server is running on port: ${port}`);
 });
