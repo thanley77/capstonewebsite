b8f08c215c5003010a1e6a57f3078f55eaf1b774
thanley77 <124112168+thanley77@users.noreply.github.com>
2023-04-23 17:00:04 -0400

    Update conn.js

--- mern/server/db/conn.js
+++ mern/server/db/conn.js
@@ -13,14 +13,14 @@ module.exports = {
       // Verify we got a good "db" object
       if (db)
       {
-        _db = db.db("pentests_ninjakiwi");
+        _db = db;
         console.log("Successfully connected to MongoDB."); 
       }
       return callback(err);
          });
   },
 
-  getDb: function () {
-    return _db;
+  getDb: function (dbName) {
+    return _db.db(dbName);
   },
 };
