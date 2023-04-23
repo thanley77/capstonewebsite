f03017dd9547dc8415fbfda055e33fcd4bc02fda
thanley77 <124112168+thanley77@users.noreply.github.com>
2023-04-23 18:33:37 -0400

    Update App.js

--- mern/client/src/App.js
+++ mern/client/src/App.js
@@ -9,7 +9,7 @@ import RecordList from "./components/recordList";
 import Edit from "./components/edit";
 import Create from "./components/create";
 import HttpxList from "./components/httpxList";
-
+import NmapList from "./components/nmapList";
 
 const App = () => {
   return (
@@ -21,6 +21,7 @@ const App = () => {
           <Route path="/edit/:id" element={<Edit />} />
           <Route path="/list" element={<Create />} />
           <Route path="/:db_name/httpx" element={<HttpxList />} />
+          <Route path="/:db_name/nmap" element={<NmapList />} />
         </Routes>
       </div>
     </div>
