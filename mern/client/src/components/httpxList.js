4295e9c2e0eefe36374152897b2f3f1364caa500
thanley77 <124112168+thanley77@users.noreply.github.com>
2023-04-23 18:11:24 -0400

    Update httpxList.js

--- mern/client/src/components/httpxList.js
+++ mern/client/src/components/httpxList.js
@@ -3,10 +3,12 @@ const Httpx = (props) => (
   <tr>
     <td>{props.httpx.input}</td>
     <td>{props.httpx.host}</td>
+    <td>{props.httpx.location}</td>
     <td>{props.httpx.scheme}</td>
     <td>{props.httpx.webserver}</td>
-    <td>{props.httpx.path}</td>
     <td>{props.httpx.status_code}</td>
+    <td>{props.httpx.final_url}</td>
+    
     <td>
       <a className="btn btn-link" href={props.httpx.url} target="_blank" rel="noopener noreferrer">View</a>
     </td>
@@ -18,7 +20,7 @@ export default function HttpxList() {
 
   useEffect(() => {
     async function getHttpxList() {
-      const response = await fetch(`/pentests_hyatt/httpx`);
+      const response = await fetch(`/:dbname/httpx`);
 
       if (!response.ok) {
         const message = `An error occurred: ${response.statusText}`;
@@ -54,11 +56,12 @@ export default function HttpxList() {
           <tr>
             <th>Domain</th>
             <th>Host</th>
+            <th>Location</th>
             <th>Scheme</th>
             <th>Webserver</th>
-            <th>Path</th>
-            <th>Status Code</th>
+            <th>StatusCode</th>
             <th>Action</th>
+            <th>FinalURL</th>
           </tr>
         </thead>
         <tbody>{httpxListTable()}</tbody>
