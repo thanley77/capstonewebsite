146bacc5ef7c200a48da8e50db4591252f06582e
thanley77 <124112168+thanley77@users.noreply.github.com>
2023-04-23 17:22:34 -0400

    Update recordList.js

--- mern/client/src/components/recordList.js
+++ mern/client/src/components/recordList.js
@@ -25,7 +25,7 @@ export default function RecordList() {
   // This method fetches the records from the database.
   useEffect(() => {
     async function getRecords() {
-      const response = await fetch(`/record/pentest_hyatt`);
+      const response = await fetch(`/record/pentests_ninjakiwi`);
 
       if (!response.ok) {
         const message = `An error occured: ${response.statusText}`;
