92ec0b277c33f96d896b2501c2ac1b98cc7434f2
thanley77 <124112168+thanley77@users.noreply.github.com>
2023-04-23 23:06:42 -0400

    Update nmapList.js

--- mern/client/src/components/nmapList.js
+++ mern/client/src/components/nmapList.js
@@ -1,102 +1,77 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from 'react-router-dom';
+import { useParams } from "react-router-dom";
 
-const Nmap = (props) => {
-  const { nmap } = props;
-  const { _id, scan } = nmap;
+const Port = (props) => (
+  <tr>
+    <td>{props.port.port_number}</td>
+    <td>{props.port.state}</td>
+    <td>{props.port.protocol}</td>
+    <td>{props.port.service}</td>
+  </tr>
+);
 
-  const portList = Object.keys(scan).map((port) => {
-    return (
-      <td key={port}>{port}</td>
-    );
-  });
+const Nmap = (props) => (
+  <tr>
+    <td>{props.nmap._id}</td>
+    <td>
+      <table>
+        <thead>
+          <tr>
+            <th>Port Number</th>
+            <th>State</th>
+            <th>Protocol</th>
+            <th>Service</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.nmap.scan.map((port) => {
+            return <Port port={port} key={port.port_number} />;
+          })}
+        </tbody>
+      </table>
+    </td>
+  </tr>
+);
 
-  const serviceList = Object.keys(scan).map((port) => {
-    return (
-      <td key={port}>{scan[port].Service}</td>
-    );
-  });
-
-  const stateList = Object.keys(scan).map((port) => {
-    return (
-      <td key={port}>{scan[port].State}</td>
-    );
-  });
-
-  return (
-    <tr>
-      <td>{_id}</td>
-      {portList}
-      {serviceList}
-      {stateList}
-    </tr>
-  );
-};
-
-export default function NmapList() {
+const NmapList = () => {
   const { db_name } = useParams();
   const [nmapList, setNmapList] = useState([]);
 
   useEffect(() => {
     async function getNmapList() {
       const response = await fetch(`/${db_name}/nmap`);
-
       if (!response.ok) {
         const message = `An error occurred: ${response.statusText}`;
         window.alert(message);
         return;
       }
-
       const nmapList = await response.json();
       setNmapList(nmapList);
     }
-
     getNmapList();
   }, [db_name]);
 
   function nmapListTable() {
-    const header = nmapList.length > 0 ? (
-      <>
-        <th>Domain</th>
-        {Object.keys(nmapList[0].scan).map((port) => (
-          <th key={port}>{port}</th>
-        ))}
-        {Object.keys(nmapList[0].scan).map((port) => (
-          <th key={`${port}_service`}>Service</th>
-        ))}
-        {Object.keys(nmapList[0].scan).map((port) => (
-          <th key={`${port}_state`}>State</th>
-        ))}
-      </>
-    ) : null;
-
-    const rows = nmapList.map((nmap) => {
-      return (
-        <Nmap
-          nmap={nmap}
-          key={nmap._id}
-        />
-      );
+    return nmapList.map((nmap) => {
+      return <Nmap nmap={nmap} key={nmap._id} />;
     });
-
-    return (
-      <>
-        <thead>
-          <tr>
-            {header}
-          </tr>
-        </thead>
-        <tbody>{rows}</tbody>
-      </>
-    );
   }
 
   return (
     <div>
       <h3>Nmap List</h3>
       <table className="table table-striped" style={{ marginTop: 20 }}>
-        {nmapListTable()}
+        <thead>
+          <tr>
+            <th>Domain</th>
+            <th>Ports</th>
+          </tr>
+        </thead>
+        <tbody>{nmapListTable()}</tbody>
       </table>
     </div>
   );
-}
+};
+
+export default NmapList;
+
