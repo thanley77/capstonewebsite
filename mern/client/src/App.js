12d2b131f3f0da25220d4d73bb27e7a929520164
thanley77 <124112168+thanley77@users.noreply.github.com>
2023-04-30 14:36:54 -0400

    Update App.js

--- mern/client/src/App.js
+++ mern/client/src/App.js
@@ -10,7 +10,6 @@ import Edit from "./components/edit";
 import Create from "./components/create";
 import HttpxList from "./components/httpxList";
 import NmapList from "./components/nmapList";
-import ImportNmapButtom from "./components/importNmapButton";
 
 const App = () => {
   return (
@@ -23,7 +22,6 @@ const App = () => {
           <Route path="/list" element={<Create />} />
           <Route path="/:db_name/httpx" element={<HttpxList />} />
           <Route path="/:db_name/nmap" element={<NmapList />} />
-          <Route path="/import-nmap" element={<ImportNmapButton />} />
         </Routes>
       </div>
     </div>
