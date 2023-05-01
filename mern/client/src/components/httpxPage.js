089c0ce6514b65127207e89afd53dfd7389628d4
thanley77 <124112168+thanley77@users.noreply.github.com>
2023-05-01 03:00:03 -0400

    Create httpxPage.js

---
+++ mern/client/src/components/httpxPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CreateHttpx from './httpxCreate';
+
+function HttpxPage() {
+  return (
+    <div>
+      <h1>HTTPX Page</h1>
+      <CreateHttpx />
+    </div>
+  );
+}
+
+export default HttpxPage;
