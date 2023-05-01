0a196fbc5ae7e46d7548f72b7a105b3d63afedde
thanley77 <124112168+thanley77@users.noreply.github.com>
2023-05-01 05:01:10 -0400

    Update httpxPage.js

--- mern/client/src/components/httpxPage.js
+++ mern/client/src/components/httpxPage.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
-import HttpxList from './HttpxList';
+import HttpxList from './httpxList';
 import CreateHttpx from './CreateHttpx';
 
 function HttpxPage() {
